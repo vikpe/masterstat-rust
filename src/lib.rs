8e8f21f9284aa8f8057c86fd43951d539b76bbf4
Viktor Persson <viktor.persson@arcsin.se>
2024-04-25 16:50:23 +0200

    Add lib.

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+mod command;
+mod server_address;
+mod udp;
+
+pub use crate::command::server_addresses;
+pub use crate::command::server_addresses_from_many;
+pub use crate::server_address::ServerAddress;
